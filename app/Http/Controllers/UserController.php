db57384d4bc203ee3743725f81673df8ecd2d465
Alan Aliaga <alanaliagadev@gmail.com>
2018-10-30 18:54:30 -0400

    [Add] UserResourceTest - it_should_contain_a_type_id_and_attributes_under_a_data_object

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Resources\UserResource;
+
 use App\User;
 
 class UserController extends Controller
 {
     public function show(User $user)
     {
-        return response()->json([
-            'data' => [
-                'type' => 'users', 'id' => (string) $user->id
-            ]
-        ], 200);
+        return new UserResource($user);
     }
 }
--- tests/Feature/UserResourceTest.php
+++ tests/Feature/UserResourceTest.php
@@ -7,10 +7,31 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\Track;
+use App\User;
 
 class UserResourceTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function it_should_contain_a_type_id_and_attributes_under_a_data_object()
+    {
+        $this->withoutExceptionHandling();
+        
+        $user = create(User::class);
+
+        $this->json('GET', $user->path())
+            ->assertJson([
+                'data' => [
+                    'type' => 'users',
+                    'id'   => (string) $user->id,
+                    'attributes' => [
+                        'username' => $user->username,
+                        'email'    => $user->email,
+                    ]
+                ]
+            ]);
+    }
 
     /** @test */
     public function a_user_identifier_should_contain_a_data_with_a_type_and_the_id_of_the_user()
